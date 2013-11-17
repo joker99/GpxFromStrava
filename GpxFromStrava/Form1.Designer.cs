a91e400a928b5b0081053a49405dcc9aa7ec48f2
joker99 <joker99@gmail.com>
2013-11-17 21:54:42 +0200

    ui designed

--- GpxFromStrava/Form1.Designer.cs
+++ GpxFromStrava/Form1.Designer.cs
@@ -28,12 +28,63 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnGetGpx = new System.Windows.Forms.Button();
+            this.txtActivityId = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnGetGpx
+            // 
+            this.btnGetGpx.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnGetGpx.Location = new System.Drawing.Point(148, 51);
+            this.btnGetGpx.Name = "btnGetGpx";
+            this.btnGetGpx.Size = new System.Drawing.Size(75, 23);
+            this.btnGetGpx.TabIndex = 0;
+            this.btnGetGpx.Text = "Get .gpx";
+            this.btnGetGpx.UseVisualStyleBackColor = true;
+            this.btnGetGpx.Click += new System.EventHandler(this.btnGetGpx_Click);
+            // 
+            // txtActivityId
+            // 
+            this.txtActivityId.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtActivityId.Location = new System.Drawing.Point(110, 10);
+            this.txtActivityId.Name = "txtActivityId";
+            this.txtActivityId.Size = new System.Drawing.Size(248, 20);
+            this.txtActivityId.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Strava activity ID:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(370, 101);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtActivityId);
+            this.Controls.Add(this.btnGetGpx);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Get .gpx from Strava Activity";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnGetGpx;
+        private System.Windows.Forms.TextBox txtActivityId;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- GpxFromStrava/Form1.cs
+++ GpxFromStrava/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,5 +17,100 @@ namespace GpxFromStrava
         {
             InitializeComponent();
         }
+
+        private void btnGetGpx_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private static string GetFireFoxCookiePath()
+        {
+            string profilePath = Environment.GetFolderPath(
+                             Environment.SpecialFolder.ApplicationData);
+            profilePath += @"\Mozilla\Firefox\Profiles\";
+
+            try
+            {
+                string[] dir = Directory.GetDirectories("*.default");
+                if (dir.Length != 1)
+                    return string.Empty;
+
+                profilePath += dir[0] + @"\" + "cookies.sqlite";
+            }
+            catch (Exception)
+            {
+                return string.Empty;
+            }
+
+            if (!File.Exists(profilePath))
+                return string.Empty;
+
+            return profilePath;
+        }
+
+        /*
+        private static bool GetCookie_FireFox(string strHost, string strField, ref string Value)
+        {
+            Value = string.Empty;
+            bool fRtn = false;
+            string strPath, strTemp, strDb;
+            strTemp = string.Empty;
+
+            // Check to see if FireFox Installed
+            strPath = GetFireFoxCookiePath();
+            if (string.Empty == strPath) // Nope, perhaps another browser
+                return false;
+
+            try
+            {
+                // First copy the cookie jar so that we can read the cookies 
+                // from unlocked copy while
+                // FireFox is running
+                strTemp = strPath + ".temp";
+                strDb = "Data Source=" + strTemp + ";pooling=false";
+
+                File.Copy(strPath, strTemp, true);
+
+                // Now open the temporary cookie jar and extract Value from the cookie if
+                // we find it.
+                using (SQLiteConnection conn = new SQLiteConnection(strDb))
+                {
+                    using (SQLiteCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandText = "SELECT value FROM moz_cookies WHERE host LIKE '%" +
+                            strHost + "%' AND name LIKE '%" + strField + "%';";
+
+                        conn.Open();
+                        using (SQLiteDataReader reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                Value = reader.GetString(0);
+                                if (!Value.Equals(string.Empty))
+                                {
+                                    fRtn = true;
+                                    break;
+                                }
+                            }
+                        }
+                        conn.Close();
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                Value = string.Empty;
+                fRtn = false;
+            }
+
+            // All done clean up
+            if (string.Empty != strTemp)
+            {
+                File.Delete(strTemp);
+            }
+            return fRtn;
+        }
+         * */
     }
 }
